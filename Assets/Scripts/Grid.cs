539dc4ad8c5255ee872d58372b5b9431375f9f5c
ArthurAD <arthurdobrya@gmail.com>
2023-12-29 17:31:46 +0200

    2nd video heat map

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,15 +1,36 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
 using CodeMonkey.Utils;
 using UnityEngine;
 
 public class Grid
 {
+    public const int HEAT_MAP_MAX_VALUE = 100;
+    public const int HEAT_MAP_MIN_VALUE = 0;
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
     private int width;
     private int height;
     private float cellSize;
     private Vector3 originPosition;
-
     private int[,] gridArray;
-    private TextMesh[,] textMeshes;
 
     public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
@@ -17,26 +38,52 @@ public class Grid
         this.height = height;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
-        gridArray = new int[width, height];
-        textMeshes = new TextMesh[width, height];
 
-        for (int x = 0; x < gridArray.GetLength(0); x++)
+        gridArray = new int[width, height];
+
+        bool showDebug = true;
+        if (showDebug)
         {
-            for (int y = 0; y < gridArray.GetLength(1); y++)
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++)
             {
-                textMeshes[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null,
-                    GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 20, Color.white,
-                    TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                for (int y = 0; y < gridArray.GetLength(1); y++)
+                {
+                    debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null,
+                        GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 30, Color.white,
+                        TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                }
             }
 
             Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
             Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+            OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) =>
+            {
+                debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y].ToString();
+            };
         }
     }
 
-    private Vector3 GetWorldPosition(int x, int y)
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int y)
     {
         return new Vector3(x, y) * cellSize + originPosition;
     }
@@ -51,8 +98,8 @@ public class Grid
     {
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
-            gridArray[x, y] = value;
-            textMeshes[x, y].text = gridArray[x, y].ToString();
+            gridArray[x, y] = Mathf.Clamp(value, HEAT_MAP_MIN_VALUE, HEAT_MAP_MAX_VALUE);
+            if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
         }
     }
 
@@ -61,6 +108,11 @@ public class Grid
         int x, y;
         GetXY(worldPosition, out x, out y);
         SetValue(x, y, value);
+    }
+
+    public void AddValue(int x, int y, int value)
+    {
+        SetValue(x, y, GetValue(x, y) + value);
     }
 
     public int GetValue(int x, int y)
@@ -80,5 +132,41 @@ public class Grid
         int x, y;
         GetXY(worldPosition, out x, out y);
         return GetValue(x, y);
+    }
+
+    public void AddValue(Vector3 worldPosition, int value, int fullValueRange, int totalRange)
+    {
+        int lowerValueAmount = Mathf.RoundToInt((float)value / (totalRange - fullValueRange));
+
+        GetXY(worldPosition, out int originX, out int originY);
+        for (int x = 0; x < totalRange; x++)
+        {
+            for (int y = 0; y < totalRange - x; y++)
+            {
+                int radius = x + y;
+                int addValueAmount = value;
+                if (radius > fullValueRange)
+                {
+                    addValueAmount -= lowerValueAmount * (radius - fullValueRange);
+                }
+
+                AddValue(originX + x, originY + y, addValueAmount);
+
+                if (x != 0)
+                {
+                    AddValue(originX - x, originY + y, addValueAmount);
+                }
+
+                if (y != 0)
+                {
+                    AddValue(originX + x, originY - y, addValueAmount);
+
+                    if (x != 0)
+                    {
+                        AddValue(originX - x, originY - y, addValueAmount);
+                    }
+                }
+            }
+        }
     }
 }
